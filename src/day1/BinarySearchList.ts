7147219b10fcbd61ac581a6aafc22a2b047b8a46
LoyalPotato <david.bazilio.r@gmail.com>
2022-10-31 20:12:03 +0000

    Binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let lo = 0;
+  let hi = haystack.length - 1;
 
+  while (lo <= hi){
+    const mid = Math.floor((hi + lo)/ 2);
+    console.log(mid)
+
+    if (needle > haystack[mid]) {
+      lo = mid + 1;
+    } else if (needle < haystack[mid]){
+      hi = mid - 1;
+    } else {
+      return true;
+    }
+    
+  }
+
+  return false;
 }
